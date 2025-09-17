76ae5db2a9c7c984b829d74f1d2c3ed7c243b583
hugoalmmm <halmeida@edenschool.fr>
2025-09-17 12:06:12 +0200

    aa

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 
 const form = document.getElementById("weatherForm");
 const resultsDiv = document.getElementById("results");
+const cityValue = document.querySelector('#cityInput')
+
+const city1 = new Promise(async (resolve, reject) => {
+    const req = await fetch(`https://wttr.in/${cityValue.value}?format=j1`);
+    const res = await req.json()
+
+    try {
+        console.log('a')
+        resolve(res)
+    } catch (error) {
+        reject(resultsDiv.innerHTML = `</p>veuillez réessayer ultérieurement</p> ${error}`)
+    }
+})
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    resultsDiv.innerHTML = "<p>Chargement...</p>";
+    Promise.all([city1])
+        .then(data => {
+            console.log(data)
+            if (data[0].current_condition[0].lang_fr[0].value === "New Found Out") {
+                resultsDiv.innerHTML = "Ville incorrect</p>"
+            } else if (data[0].current_condition[0].lang_fr[0].value === "Nuageux") {
+                console.log(data)
+                resultsDiv.innerHTML = `
+            <p>${data[0].nearest_area[0].areaName[0].value}</p>
+            <p>${data[0].current_condition[0].lang_fr[0].value}</p>
+            <p>${data[0].current_condition[0].lang_fr[0].temp_C}</p>
+            <p>${data[0].current_condition[0].lang_fr[0].value}</p>
+            <p>Prenez un menteau</p>
+            `
+            }
+        })
+        .catch(error => console.log(error))
 });
