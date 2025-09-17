715aec22e4c6e18eda2e43d63ec7ff79dac3e960
hugoalmmm <halmeida@edenschool.fr>
2025-09-17 15:02:09 +0200

    a

--- app.js
+++ app.js
@@ -3,34 +3,36 @@ const form = document.getElementById("weatherForm");
 const resultsDiv = document.getElementById("results");
 const cityValue = document.querySelector('#cityInput')
 
-const city1 = new Promise(async (resolve, reject) => {
-    const req = await fetch(`https://wttr.in/${cityValue.value}?format=j1`);
-    const res = await req.json()
 
-    try {
-        console.log('a')
-        resolve(res)
-    } catch (error) {
-        reject(resultsDiv.innerHTML = `</p>veuillez réessayer ultérieurement</p> ${error}`)
-    }
-})
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
+    const city1 = new Promise(async (resolve, reject) => {
+        const req = await fetch(`https://wttr.in/${cityValue.value}?format=j1`);
+        const res = await req.json()
+        console.log(res)
+        try {
+            console.log('a')
+            resolve(res)
+        } catch (error) {
+            reject(resultsDiv.innerHTML = `</p>veuillez réessayer ultérieurement</p> ${error}`)
+        }
+    })
+
+
     Promise.all([city1])
         .then(data => {
-            console.log(data)
-            if (data[0].current_condition[0].lang_fr[0].value === "New Found Out") {
+
+            if (data[0].nearest_area[0].areaName[0].value === "New Found Out") {
                 resultsDiv.innerHTML = "Ville incorrect</p>"
-            } else if (data[0].current_condition[0].lang_fr[0].value === "Nuageux") {
+            } else {
                 console.log(data)
                 resultsDiv.innerHTML = `
-            <p>${data[0].nearest_area[0].areaName[0].value}</p>
-            <p>${data[0].current_condition[0].lang_fr[0].value}</p>
-            <p>${data[0].current_condition[0].lang_fr[0].temp_C}</p>
-            <p>${data[0].current_condition[0].lang_fr[0].value}</p>
-            <p>Prenez un menteau</p>
-            `
+                <div class="city-card">
+                    <h2>${data[0].nearest_area[0].areaName[0].value}</h2>
+                    <p>${data[0].current_condition[0].lang_fr[0].value}</p>
+                    <p>${data[0].current_condition[0].temp_C} degres</p>
+                </div>`
             }
         })
         .catch(error => console.log(error))
