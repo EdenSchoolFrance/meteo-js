710a4d4177c77fefb85f17e563315eef0b658062
Najaxx123 <romain.brival@outlook.fr>
2025-09-18 15:01:15 +0200

    commit de romain

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
-
 const form = document.getElementById("weatherForm");
 const resultsDiv = document.getElementById("results");
+const city = form.textContent;
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    resultsDiv.innerHTML = "<p>Chargement...</p>";
+  e.preventDefault();
+  resultsDiv.innerHTML = "<p>Chargement...</p>";
+  resultsDiv.innerHTML = "<p>" + getCityWeather() + "<p>";
 });
+
+async function getCityWeather(city) {
+  const res = await fetch("https://wttr.in/Paris?format=j1");
+  const data = await res.json();
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+  return data;
+}
+
+console.log(await getCityWeather());
+
+async function forthemoment() {
+  try {
+    const data = await getCityWeather();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+console.log(city);
+
+forthemoment();
