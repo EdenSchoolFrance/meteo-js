bc1e9cf84a514434413a39394497cf678c117f56
slayne0 <majfeuguicheteau@gmail.com>
2025-09-18 14:59:50 +0200

    finis

--- app.js
+++ app.js
@@ -3,25 +3,30 @@ const form = document.getElementById("weatherForm");
 const resultsDiv = document.getElementById("results");
 const rechercher = document.querySelector(".rechercher")
 const barreDeRecherche = document.querySelector("#cityInput")
+const resultVille = document.querySelector("#results")
+
+// https://en.wikipedia.org/api/rest_v1/page/summary/Paris
 
 
 rechercher.addEventListener("click", async () => {
-    // console.log(barreDeRecherche.value + " est dans la barre de recherche ")
-    // console.log("https://wttr.in/" + barreDeRecherche.value + "?format=j1")
-    await main()
+    resultVille.textContent = ""
+    const villes = barreDeRecherche.value.split(", ")
+    for (let i = 0; i < villes.length; i++) {
+        console.log(villes)
+        await main(villes[i])
+    }
 })
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    resultsDiv.innerHTML = "<p>Chargement...</p>";
+    // resultsDiv.innerHTML = "<p>Chargement...</p>";
 });
 
 
 
-async function fetchmeteo() {
+async function fetchmeteo(ville) {
     return new Promise(async (resolve, reject) => {
-        const cityName = barreDeRecherche.value
-        const url = `https://wttr.in/${cityName}?format=j1`
+        const url = `https://wttr.in/${ville}?format=j1`
         const res = await fetch(url)
         const result = await res.json()
         resolve(result)
@@ -33,13 +38,54 @@ async function fetchmeteo() {
     })
 }
 
-async function main() {
+
+
+
+async function fetchWiki(ville) {
+    return new Promise(async (resolve, reject) => {
+        const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${ville}`
+        const res = await fetch(url)
+        const result = await res.json()
+        resolve(result)
+        if (result) {
+            resolve(result)
+        } else {
+            reject("ceci est une ereur")
+        }
+    })
+}
+
+
+
+
+async function main(ville) {
     try {
-        const data = await fetchmeteo().then(data => data)
-        console.log(data)
-        console.log(data.nearest_area[0].areaName[0].value)
-        console.log(data.current_condition[0].temp_C + " degrés")
-        console.log(data.current_condition[0].lang_fr[0].value)
+        const data = await fetchmeteo(ville).then(data => data)
+        const data2 = await fetchWiki(ville).then(data2 => data2)
+        const villeName = data.nearest_area[0].areaName[0].value
+        const degres = data.current_condition[0].temp_C + " degrés"
+        const temp = data.current_condition[0].lang_fr[0].value
+        console.log(villeName)
+        console.log(degres)
+        console.log(temp)
+        const $divVille = document.createElement("div")
+        const $villeName = document.createElement("p")
+        const $degres = document.createElement("p")
+        const $temp = document.createElement("p")
+        const $wiki = document.createElement("p")
+
+        $divVille.classList.add("divVille")
+
+        $villeName.textContent = villeName
+        $degres.textContent = degres
+        $temp.textContent = temp
+        $wiki.textContent = data2.extract
+
+        resultVille.appendChild($divVille)
+        $divVille.appendChild($villeName)
+        $divVille.appendChild($degres)
+        $divVille.appendChild($temp)
+        resultVille.appendChild($wiki)
     } catch (error) {
         console.log("erreur")
     }
