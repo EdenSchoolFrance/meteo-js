e38c00838c30e3626cfa1b7739daf6264b1dbb89
Najaxx123 <romain.brival@outlook.fr>
2025-09-19 11:56:36 +0200

    avancemant

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 const form = document.getElementById("weatherForm");
 const resultsDiv = document.getElementById("results");
-const city = form.textContent;
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
   resultsDiv.innerHTML = "<p>Chargement...</p>";
-  resultsDiv.innerHTML = "<p>" + getCityWeather() + "<p>";
+  await forthemoment();
+  //resultsDiv.innerHTML = "<p>" + getCityWeather() + "<p>";
 });
 
 async function getCityWeather(city) {
-  const res = await fetch("https://wttr.in/Paris?format=j1");
-  const data = await res.json();
   try {
+    const res = await fetch(`https://wttr.in/${city}?format=j1`);
+    const data = await res.json();
+
+    return data;
   } catch (err) {
     console.log(err);
   }
-  return data;
 }
 
-console.log(await getCityWeather());
+// console.log(await getCityWeather());
+
+function displayInformation(cityData) {
+  console.log(cityData);
+  const fetchedCityName = cityData.nearest_area[0].areaName[0].value;
+  resultsDiv.textContent = fetchedCityName;
+}
 
 async function forthemoment() {
   try {
-    const data = await getCityWeather();
-    console.log(data);
+    const cityInput = document.getElementById("cityInput");
+    const searchedCity = cityInput.value
+    const data = await getCityWeather(searchedCity);
+    displayInformation(data);
   } catch (err) {
     console.log(err);
   }
 }
-
-console.log(city);
-
-forthemoment();
